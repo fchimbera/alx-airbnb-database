cc961474c70cd215a5cf2de1f9a12254b7b1582e
fchimbera <chimberafreeman@gmail.com>
2025-07-02 01:11:58 +0200

    update readme for joins_queries.sql file

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,10 +1,15 @@
 -- This query retrieves all bookings along with the first and last names of the users who made those bookings.
 -- It uses an INNER JOIN to combine the Bookings and Users tables based on the user_id field.
-SELECT Bookings.*, Users.first_name, Users.last_name FROM Bookings INNER JOIN Users ON Bookings.user_id = Users.user_id;
+SELECT Bookings.*, Users.first_name, Users.last_name 
+FROM Bookings 
+INNER JOIN Users ON Bookings.user_id = Users.user_id;
 
 -- This query retrieves allproperties and their reviews including properties that have no reviews.
 -- It uses a LEFT JOIN to include all properties and their corresponding reviews, if available.
-SELECT Properties.*, Reviews.rating, Reviews.comment FROM Properties LEFT JOIN Reviews ON Properties.property_id = Reviews.property_id;
+SELECT Properties.*, Reviews.rating, Reviews.comment 
+FROM Properties 
+LEFT JOIN Reviews ON Properties.property_id = Reviews.property_id
+ORDER BY Properties.property_id;
 
 -- This query retrieves all users and their bookings, including users who have not made any bookings.
 SELECT
